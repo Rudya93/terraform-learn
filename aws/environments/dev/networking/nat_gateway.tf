2e1246bfe4b9e27d98fc560df375824760505179
KaterynaKupava <ezhilyaeva@gmail.com>
2022-03-13 18:33:35 +0200

    removed nat gateways

--- aws/environments/dev/networking/nat_gateway.tf
+++ aws/environments/dev/networking/nat_gateway.tf
@@ -1,3 +1,5 @@
+
+/*
 module "az_a_nat_gateway"{
   source    = "./../../../modules/networking/nat_gateway"
   subnet_id = "${module.subnet_public_a.pub_sub_id_out}"
@@ -7,3 +9,5 @@ module "az_b_nat_gateway"{
   source    = "./../../../modules/networking/nat_gateway"
   subnet_id = "${module.subnet_public_b.pub_sub_id_out}"
 }
+
+*/
